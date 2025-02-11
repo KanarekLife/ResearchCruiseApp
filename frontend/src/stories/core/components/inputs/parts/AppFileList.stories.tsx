8495160f456835f14b22f8f2957159f201df9534
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-02-11 19:57:46 +0100

    feat: add input file validation

--- frontend/src/stories/core/components/inputs/parts/AppFileList.stories.tsx
+++ frontend/src/stories/core/components/inputs/parts/AppFileList.stories.tsx
@@ -6,8 +6,16 @@ import { AppFileList } from '@/core/components/inputs/parts/AppFileList';
 const meta = {
   component: AppFileList,
   args: {
-    files: [],
-    onRemove: fn(),
+    files: [
+      {
+        name: 'File 1',
+        content: 'Content 1',
+      },
+      {
+        name: 'File 2',
+        content: 'Content 2',
+      },
+    ],
   },
 } satisfies Meta<typeof AppFileList>;
 
@@ -16,3 +24,15 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Removable: Story = {
+  args: {
+    onRemove: fn(),
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
