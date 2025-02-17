439c2c61aa73c8cbc5250defe36e9de12d20b325
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-02-17 21:01:58 +0100

    feat: add mobile table variant

---
+++ frontend/src/modules/core/components/table/mobile/AppMobileTable.tsx
@@ -0,0 +1,71 @@
+import { flexRender, HeaderContext } from '@tanstack/react-table';
+import FunnelIcon from 'bootstrap-icons/icons/funnel.svg?react';
+import React from 'react';
+
+import { AppButton } from '@/core/components/AppButton';
+import { AppModal } from '@/core/components/AppModal';
+import { AppMobileTableFilterForm } from '@/core/components/table/mobile/AppMobileTableFilterForm';
+import { TableProps } from '@/core/components/table/tableProps';
+import { createModalPortal } from '@/core/lib/utils';
+
+export function AppMobileTable<T>({ table, buttons, emptyTableMessage }: TableProps<T>) {
+  const [isFilterModalOpen, setIsFilterModalOpen] = React.useState(false);
+
+  const defaultButtons: React.ReactNode[] = [
+    <AppButton key="openFilterModalBtn" onClick={() => setIsFilterModalOpen(true)} variant="primary">
+      <FunnelIcon className="w-8 h-8" />
+    </AppButton>,
+  ];
+  const allButtons = buttons ? buttons(defaultButtons) : defaultButtons;
+
+  return (
+    <>
+      <div className="flex flex-wrap flex-col">
+        <div className="flex flex-col gap-4 m-4">
+          {allButtons.map((x, id) => (
+            // eslint-disable-next-line @eslint-react/no-array-index-key
+            <div key={id} className="flex justify-end">
+              {x}
+            </div>
+          ))}
+        </div>
+        <table className="w-full table-fixed">
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="odd:bg-gray-100 text-gray-800">
+                <td className="flex flex-col gap-2 justify-center items-center py-3">
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <div key={cell.id} className="flex justify-between items-center w-full px-5">
+                        <div className="font-bold">
+                          {flexRender(cell.column.columnDef.header, {} as HeaderContext<T, unknown>)}
+                        </div>
+                        <div>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
+                      </div>
+                    );
+                  })}
+                </td>
+              </tr>
+            ))}
+            {!!emptyTableMessage && table.getRowModel().rows.length === 0 && (
+              <tr>
+                <td colSpan={table.getAllColumns().length} className="pb-4 text-center bg-gray-100 py-3 rounded-lg">
+                  {emptyTableMessage}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      {createModalPortal(
+        <AppModal
+          title="Filtrowanie i sortowanie"
+          isOpen={isFilterModalOpen}
+          onClose={() => setIsFilterModalOpen(false)}
+        >
+          <AppMobileTableFilterForm table={table} />
+        </AppModal>
+      )}
+    </>
+  );
+}
