ea9da8a613cc540d2f300cfcfe6145689cc84399
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-02-18 21:22:18 +0100

    feat: add mobile filtering

--- frontend/src/modules/core/components/table/mobile/AppMobileTableFilterForm.tsx
+++ frontend/src/modules/core/components/table/mobile/AppMobileTableFilterForm.tsx
@@ -1,8 +1,11 @@
 import { flexRender, Header, Table } from '@tanstack/react-table';
+import { AnimatePresence, motion } from 'motion/react';
+import React from 'react';
 import { Fragment } from 'react/jsx-runtime';
 
 import { AppTableClearFiltersButton } from '@/core/components/table/common/AppTableClearFiltersButton';
 import { AppTableFilterIcon } from '@/core/components/table/common/AppTableFilterIcon';
+import { AppTableFilterList } from '@/core/components/table/common/AppTableFilterList';
 import { AppTableSortingToggle } from '@/core/components/table/common/AppTableSortingToggle';
 import { getCapabilities } from '@/core/components/table/common/utils';
 import { cn } from '@/core/lib/utils';
@@ -12,9 +15,10 @@ type Props<T> = {
 };
 export function AppMobileTableFilterForm<T>({ table }: Props<T>) {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 ">
+      <AppTableClearFiltersButton table={table} />
       {table.getHeaderGroups().map((headerGroup) => (
-        <div key={headerGroup.id} className="flex flex-col gap-4">
+        <div key={headerGroup.id} className="flex flex-col gap-4 ">
           {headerGroup.headers.map((header) => (
             <Fragment key={header.id}>
               <FormElement header={header} />
@@ -22,20 +26,22 @@ export function AppMobileTableFilterForm<T>({ table }: Props<T>) {
           ))}
         </div>
       ))}
-      <AppTableClearFiltersButton table={table} />
     </div>
   );
 }
 
-function FormElement<T>({ header }: { header: Header<T, unknown> }) {
+type FormElementProps<T> = {
+  header: Header<T, unknown>;
+};
+function FormElement<T>({ header }: FormElementProps<T>) {
   const { supportsSort, supportsFilter } = getCapabilities(header);
   if (!supportsFilter && !supportsSort) {
     return null;
   }
 
   return (
-    <div className="flex flex-col gap-6 w-full">
-      <div className="flex justify-between items-center w-full">
+    <div className="flex flex-col gap-6 border-b border-gray-300 pb-4">
+      <div className="flex justify-between items-center">
         <div>{flexRender(header.column.columnDef.header, header.getContext())}</div>
         <div className="flex flex-col gap-2">
           {supportsSort && <SortFormElement header={header} />}
@@ -46,7 +52,7 @@ function FormElement<T>({ header }: { header: Header<T, unknown> }) {
   );
 }
 
-function SortFormElement<T>({ header }: { header: Header<T, unknown> }) {
+function SortFormElement<T>({ header }: FormElementProps<T>) {
   return (
     <div
       className={cn('flex gap-2 items-center justify-end', header.column.getIsSorted() ? 'font-bold' : '')}
@@ -57,11 +63,31 @@ function SortFormElement<T>({ header }: { header: Header<T, unknown> }) {
   );
 }
 
-function FilterFormElement<T>({ header }: { header: Header<T, unknown> }) {
+function FilterFormElement<T>({ header }: FormElementProps<T>) {
+  const [expanded, setExpanded] = React.useState(false);
+
   return (
-    <div className={cn('flex gap-2 items-center justify-end', header.column.getIsFiltered() ? 'font-bold' : '')}>
-      Filtruj
-      <AppTableFilterIcon header={header} />
+    <div>
+      <div
+        className={cn('flex gap-2 items-center justify-end', header.column.getIsFiltered() ? 'font-bold' : '')}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? 'Ukryj filtry' : 'Pokaż filtry'}
+        <AppTableFilterIcon header={header} />
+      </div>
+      <AnimatePresence>
+        {expanded && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ ease: 'easeOut' }}
+            className="w-100"
+          >
+            <AppTableFilterList header={header} expanded={expanded} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
