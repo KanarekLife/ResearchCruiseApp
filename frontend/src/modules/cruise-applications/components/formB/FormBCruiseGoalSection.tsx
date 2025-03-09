a2f48ded9fb0ab8df5e9ce79c3a04faa285ec113
Stanisław Nieradko <stanislaw@nieradko.com>
2025-03-09 14:48:13 +0100

    feat: make sections in FormB look more like in FormA

--- frontend/src/modules/cruise-applications/components/formB/FormBCruiseGoalSection.tsx
+++ frontend/src/modules/cruise-applications/components/formB/FormBCruiseGoalSection.tsx
@@ -1,26 +1,29 @@
 import { AppAccordion } from '@/core/components/AppAccordion';
+import { AppDropdownInput } from '@/core/components/inputs/AppDropdownInput';
+import { AppInput } from '@/core/components/inputs/AppInput';
 import { useFormB } from '@/cruise-applications/contexts/FormBContext';
 
 export function FormBCruiseGoalSection() {
   const { formA, formAInitValues } = useFormB();
 
-  const cruiseGoal = formAInitValues.cruiseGoals.find((_, index) => index === parseInt(formA.cruiseGoal));
-
-  function getDescription() {
-    if (formA.cruiseGoalDescription.length === 0) {
-      return 'N/A';
-    }
-
-    return formA.cruiseGoalDescription;
-  }
-
   return (
     <AppAccordion title="6. Cel rejsu" expandedByDefault>
-      <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-2">
-        <div>Cel rejsu:</div>
-        <div className="font-semibold">{cruiseGoal}</div>
-        <div>Opis:</div>
-        <div className="font-semibold">{getDescription()}</div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div>
+          <AppDropdownInput
+            name="cruiseGoal"
+            value={formA.cruiseGoal}
+            label="Cel rejsu"
+            allOptions={formAInitValues.cruiseGoals.map((cruiseGoal, index) => ({
+              value: index.toString(),
+              inlineLabel: cruiseGoal,
+            }))}
+            disabled
+          />
+        </div>
+        <div>
+          <AppInput name="cruiseGoalDescription" value={formA.cruiseGoalDescription} label="Opis" disabled />
+        </div>
       </div>
     </AppAccordion>
   );
--- frontend/src/modules/cruise-applications/components/formB/FormBShipUsageSection.tsx
+++ frontend/src/modules/cruise-applications/components/formB/FormBShipUsageSection.tsx
@@ -1,26 +1,27 @@
 import { AppAccordion } from '@/core/components/AppAccordion';
+import { AppDropdownInput } from '@/core/components/inputs/AppDropdownInput';
+import { AppInput } from '@/core/components/inputs/AppInput';
 import { useFormB } from '@/cruise-applications/contexts/FormBContext';
 
 export function FormBShipUsageSection() {
   const { formA, formAInitValues } = useFormB();
 
-  const predefinedUsage = formAInitValues.shipUsages.at(parseInt(formA.shipUsage ?? '0'));
-
-  function getDifferentUsage() {
-    if (formA.differentUsage !== '4' || formA.differentUsage.length === 0) {
-      return 'N/A';
-    }
-
-    return formA.differentUsage;
-  }
-
   return (
     <AppAccordion title="3. Sposób wykorzystania statku" expandedByDefault>
-      <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-2">
-        <div className="font-semibold">Statek na potrzeby badań będzie wykorzystywany:</div>
-        <div>{predefinedUsage}</div>
-        <div className="font-semibold">Inny sposób użycia:</div>
-        <div>{getDifferentUsage()}</div>
+      <div className="grid grid-cols-1 gap-4">
+        <AppDropdownInput
+          name="shipUsage"
+          value={formA.shipUsage ?? ''}
+          allOptions={formAInitValues?.shipUsages.map((shipUsage, i) => ({
+            value: i.toString(),
+            inlineLabel: shipUsage,
+          }))}
+          label="Statek na potrzeby badań będzie wykorzystywany"
+          disabled
+        />
+        {formA.shipUsage === '4' && (
+          <AppInput name="differentUsage" value={formA.differentUsage} label="Inny sposób użycia" disabled />
+        )}
       </div>
     </AppAccordion>
   );
