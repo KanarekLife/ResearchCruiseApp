0746efc2ed6acc6a54c7f18ae4a38ad210479898
ketrab <bartekkrawisz@wp.pl>
2025-03-16 21:19:43 +0100

    fix: application details layout

--- frontend/src/modules/cruise-applications/components/application-details/ApplicationDetailsInformationSection.tsx
+++ frontend/src/modules/cruise-applications/components/application-details/ApplicationDetailsInformationSection.tsx
@@ -2,6 +2,7 @@ import { AppAccordion } from '@/core/components/AppAccordion';
 import { AppLink } from '@/core/components/AppLink';
 import { AppInput } from '@/core/components/inputs/AppInput';
 import { AppNumberInput } from '@/core/components/inputs/AppNumberInput';
+import { AppDatePickerInput } from '@/core/components/inputs/dates/AppDatePickerInput';
 import { useApplicationDetails } from '@/cruise-applications/contexts/ApplicationDetailsContext';
 
 export function ApplicationDetailsInformationSection() {
@@ -11,7 +12,7 @@ export function ApplicationDetailsInformationSection() {
     <AppAccordion title="1. Informacje o zgłoszeniu" expandedByDefault>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <AppInput name="number" value={application.number} label="Numer zgłoszenia:" required disabled />
-        <AppInput name="date" value={application.date} label="Data wysłania:" required disabled />
+        <AppDatePickerInput name="date" value={application.date} label="Data wysłania:" required disabled />
         <AppInput name="year" value={`${application.year}`} label="Rok rejsu:" required disabled />
         <AppInput
           name="cruiseLeader"
