9987fcb6a854fb3ae97f242d79c67ba30456e9e4
ketrab <bartekkrawisz@wp.pl>
2025-03-22 12:57:26 +0100

    feat: add basic form c structure

---
+++ frontend/src/modules/cruise-applications/components/formC/FormC.tsx
@@ -0,0 +1,21 @@
+import { FormCContextType, FormCProvider } from '@/cruise-applications/contexts/FormCContext';
+
+type Props = {
+  context: FormCContextType & {
+    onSubmit: () => void;
+    onSaveDraft: () => void;
+  };
+};
+export function FormC({ context }: Props) {
+  function onSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    context.onSubmit();
+  }
+
+  return (
+    <FormCProvider value={context}>
+      <form className="space-y-8" onSubmit={onSubmit}>
+      </form>
+    </FormCProvider>
+  );
+}
