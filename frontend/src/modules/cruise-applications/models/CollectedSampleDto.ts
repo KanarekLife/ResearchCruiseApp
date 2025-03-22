af57cd1071fae9562ef74d47ac14c177c2054b01
ketrab <bartekkrawisz@wp.pl>
2025-03-22 12:57:05 +0100

    feat: add support for form c api calls

---
+++ frontend/src/modules/cruise-applications/models/CollectedSampleDto.ts
@@ -0,0 +1,6 @@
+export type CollectedSampleDto = {
+  type: string; // Max length 1024
+  amount: string; // Max length 1024
+  analysis: string; // Max length 1024
+  publishing: string; // Max length 1024
+};
---
+++ frontend/src/modules/cruise-applications/models/ResearchTaskEffectDto.ts
@@ -0,0 +1,19 @@
+export interface ResearchTaskEffectDto {
+  type: string;
+  title?: string;
+  magazine?: string;
+  author?: string;
+  institution?: string;
+  date?: string;
+  startDate?: string;
+  endDate?: string;
+  financingAmount?: string;
+  financingApproved?: string;
+  description?: string;
+  securedAmount?: string;
+  ministerialPoints?: string;
+  done: string;
+  publicationMinisterialPoints?: string;
+  managerConditionMet: string;
+  deputyConditionMet: string;
+}
