effb38a749bf46e613a73dd95d4cc6cda996dce7
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-05-03 19:42:24 +0200

    feat: add basic otel support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,10 @@ import { createRoot } from 'react-dom/client';
 
 import { AppContextProvider } from '@/core/providers/AppContextProvider';
 import CustomRouterProvider from '@/core/providers/CustomRouterProvider';
+import { initializeFaro } from '@/monitoring/lib/grafanaFaro';
 import { UserContextProvider } from '@/user/providers/UserContextProvider';
+
+initializeFaro();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
