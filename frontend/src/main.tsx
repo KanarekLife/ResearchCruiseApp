e8c9864d32652bedb04be5fb232d128165460810
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-05-12 21:50:17 +0200

    chore: move grafana faro initialization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { initializeFaro } from '@/core/lib/grafanaFaro';
 import { AppContextProvider } from '@/core/providers/AppContextProvider';
 import CustomRouterProvider from '@/core/providers/CustomRouterProvider';
-import { initializeFaro } from '@/monitoring/lib/grafanaFaro';
 import { UserContextProvider } from '@/user/providers/UserContextProvider';
 
 initializeFaro();
